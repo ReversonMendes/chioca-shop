a9b62918156a11ccf00a2a46b61a8b29360e08c3
Reverson Mendes <reverson.mendes@totvs.com.br>
2021-10-08 13:42:50 -0300

    inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCXVTjTuBUXf-AIzaSyB-JDTf1AFT5JseD0MBXjbcGc1xZlsg0Qs',
+    authDomain: 'chioca-app-shop.firebaseapp.com',
+    databaseURL: 'https://chioca-app-shop-default-rtdb.firebaseio.com/',
+    projectId: 'chioca-app-shop',
+    storageBucket: 'chioca-app-shop.appspot.com',
+    messagingSenderId: '548089684170'
+  }
+};
